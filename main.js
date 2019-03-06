7d82fd7b7f70aedae971d637f74cc2498f9bf657
nikitaChihranov <nikita.chihranov@gmail.com>
2019-03-07 01:37:32 +0400

    Add script to handling of play, pause, stop, previous, and next buttons.

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ let audio;
 $('#pause').hide();
 
 playAudio($('.playlist li:first-child'));
-function playAudio(el){
+
+function playAudio(el) {
     let song = el.attr('song');
     let title = el.text();
     let artist = el.attr('artist');
@@ -10,7 +11,7 @@ function playAudio(el){
 
     audio = new Audio('media/' + song);
 
-    if(!audio.currentTime){
+    if (!audio.currentTime) {
         $('.duration').html('0.00');
     }
     $('.audio-player .title').text(title);
@@ -18,4 +19,61 @@ function playAudio(el){
     $('img.cover').attr('src', 'photos/covers/' + cover);
     $('.playlist li').removeClass('active');
     el.addClass('active');
+}
+
+$('#play').click(() => {
+    audio.play();
+    $('#play').hide();
+    $('#pause').show();
+    $('.duration').fadeIn(400);
+    showDuration();
+});
+$('#pause').click(() => {
+    audio.pause();
+    $('#pause').hide();
+    $('#play').show();
+    showDuration();
+});
+$('#stop').click(() => {
+    audio.pause();
+    audio.currentTime = 0;
+    $('#pause').hide();
+    $('#play').show();
+    $('.duration').fadeOut(400);
+});
+$('#next').click(() => {
+    audio.pause();
+    let next = $('.playlist li.active').next();
+    if(next.length == 0) {
+        next = $('.playlist li:first-child');
+    }
+    playAudio(next);
+    audio.play();
+    showDuration();
+});
+$('#prev').click(() => {
+        audio.pause();
+        let prev = $('.playlist li.active').prev();
+        if (prev.length == 0) {
+            prev = $('.playlist li:last-child');
+        }
+        playAudio(prev);
+        audio.play();
+        showDuration();
+});
+
+function showDuration() {
+    $(audio).bind('timeupdate', () => {
+        let s = parseInt(audio.currentTime % 60);
+        let m = parseInt((audio.currentTime / 60) % 60);
+        if (s < 10) {
+            s = '0' + s;
+        }
+        $('.duration').html(m + '.' + s);
+        let v = 0;
+        if(audio.currentTime>0){
+            v = Math.floor((100/audio.duration) * audio.currentTime);
+        }
+        $('.progress').css('width',v+'%');
+    })
 }
